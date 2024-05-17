3038686b8715141412c207e3b823674447282a27
masgal <dimasrumekso28@gmail.com>
2024-05-17 23:09:27 +0700

    solution3

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,17 @@
 def playing_domino(cards, deck):
-    return []
+    max_card = None
+    for card in cards:
+        if card[0] == deck[0] or card[1] == deck[0]:
+            if max_card is None or max(card) > max(max_card):
+                max_card = card
+        elif card[0] == deck[1] or card[1] == deck[1]:
+            if max_card is None or max(card) > max(max_card):
+                max_card = card
+
+    if max_card is not None:
+        return max_card
+    else:
+        return []
 
 if __name__ == "__main__":
     print(playing_domino([[6, 5], [3, 4], [2, 1], [3, 3]], [4, 3]))  # [3, 4]
